344500b562ce3147f5f6b336c68343e6c2735a22
kim sunjin <rlatjswls14@gmail.com>
2026-03-19 11:38:32 +0900

    카드와 덱

---
+++ MyGame/Card.cs
@@ -0,0 +1,23 @@
+﻿using Framework.Engine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+internal class Card
+{
+    public string Suit { get; set; }
+    public int Number { get; set; }
+
+
+    public Card (string suit,int number)
+    {
+        Number = number;
+        Suit = suit;
+    }
+
+    public override string ToString()
+    {
+        return $"{Suit}{Number}";
+    }
+}
--- MyGame/Deck.cs
+++ MyGame/Deck.cs
@@ -1,11 +1,70 @@
 ﻿using Framework.Engine;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
-internal class Deck 
+internal class Deck
 {
+    private List<Card> cards;
+
+    public Deck()
+    {
+        cards = new List<Card>();
+        NewDeck();
+    }
+
+    private void Make()
+    {
+        string[] suits = { "◆", "●", "▲" };
+
+        foreach (var suit in suits)
+        {
+            for (int i = 1; i <= 8; i++)
+            {
+                cards.Add(new Card(suit, i));
+            }
+        }
+    }
+
+    private void NewDeck()
+    {
+        cards.Clear();
+        Make();
+
+        for (int i = cards.Count - 1; i > 1; i--)
+        {
+            Random random = new Random();
+            int x = random.Next(0, i + 1);
+
+            var temp = cards[i];
+            cards[i] = cards[x];
+            cards[x] = temp;
+        }
+    }
+
+    public Card Draw()
+    {
+        Card card = cards[0];
+        cards.RemoveAt(0);
+        return card;
+    }
+
+    public void AllDraw(List<Player> players)
+    {
+        int Playercount = 0;
+        while (cards.Count > 0)
+        {
+            players[Playercount].AddCard(Draw());
+            Playercount= (Playercount + 1)%players.Count;
+        }
+
+    }
+
+
+
 
 }
 
